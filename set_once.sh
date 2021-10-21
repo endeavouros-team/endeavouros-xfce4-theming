69778aa58e1ff7e185a7ae38496f7dafb75fd2d8
Johannes Kamprad <archlinux@kamprad.net>
2021-10-21 20:57:03 +0200

    Update set_once.sh

--- set_once.sh
+++ set_once.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 dbus-launch dconf load / < ~/xed.dconf
-dbus-launch dconf load / < ~/mousepad.dconf
-rm ~/xed.dconf ~/mousepad.dconf ~/.config/autostart/set_once.desktop ~/set_once.sh 
+rm ~/xed.dconf ~/.config/autostart/set_once.desktop ~/set_once.sh 
