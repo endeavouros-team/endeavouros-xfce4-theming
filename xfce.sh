37e230a11c637359b43886abd00b68ca9092b83e
Johannes Kamprad <archlinux@kamprad.net>
2021-10-27 12:10:06 +0200

    Update xfce.sh
    Now takes packages from default list repository, changed to use slick greeter for Lightdm

--- xfce.sh
+++ xfce.sh
@@ -1,16 +1,24 @@
 #bin/bash
 
-echo "******* Installing EndeavourOS Theming for XFCE4*******"
+echo "******* Installing EndeavourOS Theming for XFCE4*******" && sleep 5
 
 echo "******* cloning dotfiles for EndeavourOS - XFCE4 Theming *******" && sleep 1
     git clone https://github.com/endeavouros-team/endeavouros-xfce4-theming.git
     cd endeavouros-xfce4-theming
-echo "******* Getting theme packages installed now: *******" && sleep 1
-    sudo pacman -S --noconfirm --needed - < xfce4-packages-list
+echo "******* Getting theme and packages installed now: *******" && sleep 1
+    wget -q --timeout=10https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-packages-lists/master/xfce4
+    sudo pacman -S --noconfirm --needed - < xfce4
 
 echo "******* setting up theme for Light-DM: *******" && sleep 1
-    wget -q --timeout=10 https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-iso-next/main/airootfs/etc/lightdm/lightdm-gtk-greeter.conf
-    sudo cp  lightdm-gtk-greeter.conf /etc/lightdm/
+    wget -q --timeout=10 https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/airootfs/etc/lightdm/slick-greeter.conf
+    sudo cp  slick-greeter.conf /etc/lightdm/
+    
+echo "******* set lightdm.conf to logind-check-graphical=true ... enable slick greeter to be used *******" && sleep 1
+    sed -i 's?#logind-check-graphical=false?logind-check-graphical=true?' /etc/lightdm/lightdm.conf
+    sed -i 's?#greeter-session=example-gtk-gnome?greeter-session=lightdm-slick-greeter?' /etc/lightdm/lightdm.conf
+    sed -i 's?#allow-user-switching=true?allow-user-switching=true?' /etc/lightdm/lightdm.conf
+
+echo "******* enable lightdm to be used on boot *******" && sleep 1    
     sudo systemctl -f enable lightdm
       
 echo "******* setting up xfce4 theme and settings: *******" && sleep 1
@@ -20,8 +28,8 @@ echo "******* setting up xfce4 theme and settings: *******" && sleep 1
     cd ..
     rm -rf endeavouros-xfce4-theming 
 
-echo "******* All Done --- restarting System NOW! *******"
-echo "******* Please login again and enjoy EndeavourOS Theming! *******"
+echo "******* All Done --- restarting System NOW! *******" && sleep 10
+echo "******* Please login again and enjoy EndeavourOS Theming! *******" && sleep 1
 
     yad --title="Restarting System" \
         --text="All done --- please login again and enjoy XFCE4 with EndeavourOS Theming!" \
