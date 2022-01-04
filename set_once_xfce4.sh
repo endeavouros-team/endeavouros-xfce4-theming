783b3cb35e9ec60a4aa7e5fa27ee23aade81b0df
Johannes Kamprad <archlinux@kamprad.net>
2022-01-04 15:16:54 +0100

    Update set_once_xfce4.sh
    set_once_xfce4.desktop

--- set_once_xfce4.sh
+++ set_once_xfce4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 dbus-launch dconf load / < ~/xed.dconf
-rm ~/xed.dconf ~/.config/autostart/set_once.desktop ~/set_once.sh 
+rm ~/xed.dconf ~/.config/autostart/set_once_xfce4.desktop ~/set_once_xfce4.sh 
