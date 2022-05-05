fe9e56ac9b1df330846438c39d641b25e5c2df9e
Johannes Kamprad <archlinux@kamprad.net>
2022-05-05 20:09:44 +0200

    Update set_once_xfce4.sh
    adding fix fro firewall-applet to not autostart

--- set_once_xfce4.sh
+++ set_once_xfce4.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 dbus-launch dconf load / < ~/xed.dconf
+cp /etc/xdg/autostart/firewall-applet.desktop ~/.config/autostart/
+echo Hidden=true >> ~/.config/autostart/firewall-applet.desktop
 rm ~/xed.dconf ~/.config/autostart/set_once_xfce4.desktop ~/set_once_xfce4.sh 
