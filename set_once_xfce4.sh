e578bd4d48c66daeea284f8a00a790f31b2f4d07
Johannes Kamprad <archlinux@kamprad.net>
2022-05-06 23:53:03 +0200

    Update set_once_xfce4.sh
    chmod +x ~/.config/autostart/firewall-applet.desktop

--- set_once_xfce4.sh
+++ set_once_xfce4.sh
@@ -2,4 +2,5 @@
 dbus-launch dconf load / < ~/xed.dconf
 cp /etc/xdg/autostart/firewall-applet.desktop ~/.config/autostart/
 echo Hidden=true >> ~/.config/autostart/firewall-applet.desktop
+chmod +x ~/.config/autostart/firewall-applet.desktop
 rm ~/xed.dconf ~/.config/autostart/set_once_xfce4.desktop ~/set_once_xfce4.sh 
